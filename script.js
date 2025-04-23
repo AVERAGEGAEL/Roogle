16cf0eb3b89106673975d4c0699c5070bc54df9f
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-04-23 14:25:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
-let cloakTab = null;
+const cloakOptions = ["No Cloak", "Google Docs", "Classroom", "Clever"];
+let currentCloakIndex = 0;
 
-function setCloak(option) {
-  cloakTab = option;
-  document.title = option ? option : "Google";
+function slideCloak(direction) {
+  currentCloakIndex = (currentCloakIndex + direction + cloakOptions.length) % cloakOptions.length;
+  document.getElementById("cloak-label").innerText = cloakOptions[currentCloakIndex];
 }
 
-document.getElementById("search-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  let url = document.getElementById("url-input").value.trim();
+document.getElementById("search-form").addEventListener("submit", function(event) {
+  event.preventDefault();
 
+  let url = document.getElementById("url-input").value;
   if (!url.startsWith("http://") && !url.startsWith("https://")) {
     url = "https://" + url;
   }
 
-  const spinner = document.getElementById("loading-spinner");
-  const iframe = document.getElementById("proxy-iframe");
-
-  spinner.style.display = "block";
-  iframe.style.display = "none";
+  const iframeContainer = document.getElementById("iframe-container");
+  iframeContainer.innerHTML = "";
+  const iframe = document.createElement("iframe");
   iframe.src = url;
 
   iframe.onload = function () {
-    spinner.style.display = "none";
+    document.getElementById("loading-spinner").style.display = "none";
     iframe.style.display = "block";
   };
 
-  if (cloakTab) {
+  iframeContainer.appendChild(iframe);
+
+  // Spinner
+  const spinner = document.getElementById("loading-spinner");
+  spinner.style.display = "block";
+  const loadEstimate = document.getElementById("load-estimate");
+  loadEstimate.innerText = "Estimating load time...";
+  setTimeout(() => {
+    loadEstimate.innerText = "Should load in 5-10 seconds...";
+  }, 1000);
+
+  // Cloak
+  const cloakChoice = cloakOptions[currentCloakIndex];
+  if (cloakChoice !== "No Cloak") {
     const newWindow = window.open("about:blank", "_blank");
-    const cloakHTML = `
-      <html>
-        <head>
-          <title>${cloakTab}</title>
-          <link rel="icon" href="Google.png" type="image/png" />
-        </head>
-        <body style="margin:0;padding:0;overflow:hidden;">
-          <iframe src="${url}" style="border:0;width:100vw;height:100vh;"></iframe>
-        </body>
-      </html>`;
-    newWindow.document.write(cloakHTML);
-    newWindow.document.close();
+    if (newWindow) {
+      const cloakHTML = `
+        <html>
+          <head><title>${cloakChoice}</title><link rel="icon" href="${location.origin}/Google.png" /></head>
+          <body style="margin:0;overflow:hidden;">
+            <iframe src="${url}" style="border:none;width:100vw;height:100vh;"></iframe>
+          </body>
+        </html>`;
+      newWindow.document.write(cloakHTML);
+      newWindow.document.close();
+    }
   }
 });
