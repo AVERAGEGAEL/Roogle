958f7304bad7f9d010f465a16897dbea701f825b
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-04-23 14:42:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-document.getElementById("search-form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  const urlInput = document.getElementById("url-input");
-  const iframe = document.getElementById("proxy-iframe");
-  const iframeContainer = document.getElementById("iframe-container");
-  const spinner = document.getElementById("loading-spinner");
-  let url = urlInput.value.trim();
+let cloakTitle = "";
 
-  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+document.getElementById("cloak-select").addEventListener("change", (e) => {
+  cloakTitle = e.target.value;
+  if (!cloakTitle) document.title = "Roogle";
+});
+
+document.getElementById("search-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let url = document.getElementById("url-input").value.trim();
+  if (!url.startsWith("http")) {
     url = "https://" + url;
   }
 
-  // Show loading spinner
+  const iframe = document.getElementById("proxy-frame");
+  const spinner = document.getElementById("loading-spinner");
+
   spinner.style.display = "block";
   iframe.style.display = "none";
 
-  iframe.src = url;
-
-  iframe.onload = () => {
+  iframe.onload = function () {
     spinner.style.display = "none";
     iframe.style.display = "block";
   };
 
-  const cloakOption = document.getElementById("cloak-slider").value;
-  if (cloakOption) {
+  iframe.src = url;
+
+  if (cloakTitle) {
     const newWindow = window.open("about:blank", "_blank");
     const cloakHTML = `
       <html>
-        <head>
-          <title>${cloakOption}</title>
-          <link rel="icon" href="${location.origin}/Google.png" />
-        </head>
-        <body style="margin:0;overflow:hidden">
+        <head><title>${cloakTitle}</title><link rel="icon" href="${location.origin}/Google.png" /></head>
+        <body style="margin:0;overflow:hidden;">
           <iframe src="${url}" style="width:100vw;height:100vh;border:none;"></iframe>
         </body>
-      </html>
-    `;
+      </html>`;
     newWindow.document.write(cloakHTML);
     newWindow.document.close();
   }
