1eef1ae0b8ed357c8e316743b96deaf8b13a45af
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-04-28 10:56:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 document.getElementById('proxyForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    const url = document.getElementById('url').value;
-    const recaptchaResponse = grecaptcha.getResponse(); // Get reCAPTCHA response token
+    const urlInput = document.getElementById('url').value;
     const iframe = document.getElementById('proxyIframe');
     const iframeContainer = document.getElementById('iframe-container');
-    
-    if (!recaptchaResponse) {
-        alert("Please complete the reCAPTCHA.");
-        return;
-    }
 
-    if (url) {
-        const proxyUrl = `https://fallen-america.uraverageopdoge.workers.dev/?url=${encodeURIComponent(url)}&recaptchaResponse=${recaptchaResponse}`;
+    if (urlInput) {
+        const proxyUrl = `https://fallen-america.uraverageopdoge.workers.dev/?url=${encodeURIComponent(urlInput)}`;
         iframe.src = proxyUrl;
         iframeContainer.style.display = 'block';
     }
